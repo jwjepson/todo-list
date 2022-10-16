d0e3437f2dc8b060e3c10b6d12629d4b2fc346ff
Jake Jepson <jwjepson1@gmail.com>
2022-10-15 22:26:24 -0400

    add style & css loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,16 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+     {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+     },
+    ],
+  },
 };
