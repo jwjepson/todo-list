88324a08308fb799948074506528c99a591b1591
Jake Jepson <jwjepson1@gmail.com>
2022-10-17 15:38:36 -0400

    display add task popup & test creating new task object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+import {Task} from "./todos";
+
+const defaultTasks = [];
+
+const task1 = new Task("Test Task", "This is a test Task", "10/27/2022", "high");
+defaultTasks.push(task1);
+
+const todoItems = document.querySelector(".todo-items");
+
+defaultTasks.forEach((task) => {
+    const el = document.createElement("li");
+    el.textContent = task.description;
+    todoItems.appendChild(el);
+});
+
+const addTaskButton = document.querySelector(".add-task");
+const taskForm = document.querySelector(".add-task-form");
+const taskOverlay = document.querySelector(".opacity-cover");
+
+addTaskButton.addEventListener("click", () => {
+    taskForm.style.display = "block";
+    taskOverlay.style.display = "block";
+});
