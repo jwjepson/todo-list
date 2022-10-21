7ed771c0eaab65b3f528e2d653603c072c56cbb9
Jake Jepson <jwjepson1@gmail.com>
2022-10-20 21:38:44 -0400

    add class to change complete task styling

--- src/complete.js
+++ src/complete.js
@@ -2,17 +2,21 @@ import {defaultTasks} from "./index";
 
 function markComplete() {
     const task = defaultTasks[this.parentNode.parentNode.getAttribute("data-index")];
+    const task_el = this.parentNode.parentNode;
     const taskButton = this;
 
     if (task.status == "incomplete") {
         task.status = "complete";
         taskButton.classList.add("checked");
+        task_el.classList.add("complete");
     }
     else {
         task.status = "incomplete";
         taskButton.classList.remove("checked");
-        
+        task_el.classList.remove("complete");       
     }
+    console.log(task);
+    console.log(task_el);
 }
 
 export {markComplete};
