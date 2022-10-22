23666d90d7b58132f011324a1d815243e6b8b4a3
Jake Jepson <jwjepson1@gmail.com>
2022-10-22 17:02:17 -0400

    create Add Task Button for adding a new task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import {Task} from "./todos";
 import {markComplete} from "./complete";
+import {editTask} from "./edit";
 
 const defaultTasks = [];
 
@@ -24,6 +25,7 @@ function renderTasks() {
         li.querySelector(".task-description").textContent = task.description;
         li.querySelector(".task-dueDate").textContent = task.dueDate;
         li.querySelector(".task-complete-button").addEventListener("click", markComplete);
+        li.querySelector("#edit-button").addEventListener("click", editTask);
         li.style.display = "flex";
         li.className = "task";
         if (task.status == "complete") {
@@ -33,33 +35,36 @@ function renderTasks() {
         li.dataset.index = index;
         docFrag.appendChild(li);
         todoItems.appendChild(docFrag);
-        console.log(defaultTasks);
     });
 }
 
-// Add a new task
-const submitTaskButton = document.querySelector("#submit-task-button");
-submitTaskButton.addEventListener("click", () => {
-    const title = document.querySelector("#title").value;
-    const description = document.querySelector("#description").value;
-    const dueDate = document.querySelector("#date").value;
-    const priority = document.querySelector("input[name=priority]:checked").value;
-    const task = new Task(title, description, dueDate, priority, "incomplete");
-    defaultTasks.push(task);
-    taskForm.style.display = "none";
-    taskOverlay.style.display = "none";
-    renderTasks();
-})
-
-
 // Displaying the Add Task popup
-const addTaskButton = document.querySelector(".add-task");
+
+const addTask = document.querySelector(".add-task");
 const taskForm = document.querySelector(".add-task-form");
 const taskOverlay = document.querySelector(".opacity-cover");
+const form = document.querySelector("form");
 
-addTaskButton.addEventListener("click", () => {
+addTask.addEventListener("click", () => {
+    const addTaskButton = document.createElement("button");
+    addTaskButton.classList.add("add-task-button");
+    addTaskButton.textContent = "Add Task";
+    addTaskButton.setAttribute("type", "button");
+    addTaskButton.addEventListener("click", () => {
+        const title = document.querySelector("#title").value;
+        const description = document.querySelector("#description").value;
+        const dueDate = document.querySelector("#date").value;
+        const priority = document.querySelector("input[name=priority]:checked").value;
+        const task = new Task(title, description, dueDate, priority, "incomplete");
+        defaultTasks.push(task);
+        taskForm.style.display = "none";
+        taskOverlay.style.display = "none";
+        renderTasks();
+        form.reset();
+    })
+    form.appendChild(addTaskButton);
     taskForm.style.display = "block";
     taskOverlay.style.display = "block";
 });
 
-export {defaultTasks};
+export {defaultTasks, renderTasks};
