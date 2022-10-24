85ce420f427b2b0850220342216e5af8215827e9
Jake Jepson <jwjepson1@gmail.com>
2022-10-23 23:23:45 -0400

    add project constructor to Task object

--- src/todos.js
+++ src/todos.js
@@ -1,9 +1,10 @@
-function Task(title, description, dueDate, priority, status) {
+function Task(title, description, dueDate, priority, status, project) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.status = status;
+    this.project = project;
 }
 
 export {Task};
