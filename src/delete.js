d2cb1e91abad3208269852b8f725f611c05c704e
Jake Jepson <jwjepson1@gmail.com>
2022-10-25 16:47:58 -0400

    update localStorage on delete

--- src/delete.js
+++ src/delete.js
@@ -3,6 +3,7 @@ import {defaultTasks, renderAllTasks} from "./index";
 
 function deleteTask() {
     defaultTasks.splice(this.parentNode.parentNode.getAttribute("data-index"), 1);
+    localStorage.setItem("defaultTasks", JSON.stringify(defaultTasks));
     renderAllTasks();
 }
 
